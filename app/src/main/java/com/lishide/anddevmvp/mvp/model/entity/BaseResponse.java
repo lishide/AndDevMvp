9e1fbc202f6e95f8e4792c4a83ac0fc7a4a20db1
lishide <lishidezy@qq.com>
2018-09-30 20:10:34 +0800

    upgrade gradle version.

--- app/src/main/java/com/lishide/anddevmvp/mvp/model/entity/BaseResponse.java
+++ app/src/main/java/com/lishide/anddevmvp/mvp/model/entity/BaseResponse.java
@@ -6,12 +6,12 @@ import java.io.Serializable;
 
 /**
  * 如果你服务器返回的数据格式固定为这种方式（这里只提供思想，服务器返回的数据格式可能不一致，可根据自家服务器返回的格式作更改）
- * 替换范型即可重用 {@link BaseJson}
+ * 指定范型即可改变 {@code data} 字段的类型, 达到重用 {@link BaseResponse}, 如果你实在看不懂, 请忽略
  *
  * @author lishide
  * @date 2017/11/09
  */
-public class BaseJson<T> implements Serializable {
+public class BaseResponse<T> implements Serializable {
     private T data;
     private String code;
     private String msg;
--- gankarms/src/main/java/com/lishide/gankarms/mvp/model/entity/BaseResponse.java
+++ gankarms/src/main/java/com/lishide/gankarms/mvp/model/entity/BaseResponse.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 /**
  * 如果你服务器返回的数据格式固定为这种方式（这里只提供思想，服务器返回的数据格式可能不一致，可根据自家服务器返回的格式作更改）
- * 替换范型即可重用 {@link BaseResponse}
+ * 指定范型即可改变 {@code data} 字段的类型, 达到重用 {@link BaseResponse}, 如果你实在看不懂, 请忽略
  *
  * @author lishide
  * @date 2017/11/09
